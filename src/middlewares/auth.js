8c57c1b5742f676a847bd0130b09d33f7d63d09a
Pratyush Sharma <pratyushsharma25feb@gmail.com>
2025-12-06 00:19:56 +0530

    Updated auth middleware to return 401 with login message instead of throwing an error

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,7 @@ const userAuth=async (req, res, next)=>{
     try{
         const token=req.cookies.token;
         if(!token){
-            throw new Error("token not found");
+            return res.status(401).send("Please Login!");
         }
         const decodedMsg=jwt.verify(token, "lawdeKaSecret");
         const _id=decodedMsg._id;
