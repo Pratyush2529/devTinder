891e8b30bea3d15b1c1be8497b576caaee4f774c
Pratyush Sharma <pratyushsharma25feb@gmail.com>
2025-12-10 01:13:12 +0530

    added port to env

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use("/", userRouter);
 connectDB()
 .then(()=>{
     console.log("Database connected");
-    app.listen(7777, () => {
+    app.listen(process.env.PORT, () => {
         console.log("Server started on port 7777");
     })
     
